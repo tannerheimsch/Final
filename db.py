e25de716215afd22190f0aaca5af5054dc17d4bb
tannerheimsch <h3imscht@outlook.com>
2023-12-12 21:39:52 -0600

    db.py finished, ui updated - added ability to import from certain information from a Discogs .csv

--- db.py
+++ db.py
@@ -1,12 +1,71 @@
 import sqlite3
+import csv
+import os
 from contextlib import closing
-#from business import
+from business import Vinyl
 
+DATABASE = "Final.sqlite"  # Database Name
 
-DATABASE = "Final.sqlite" # Database Name
-
+# Function to establish database connection
 def connect():
     return sqlite3.connect(DATABASE)
 
+# Function to close database connection
 def close(connection):
     connection.close()
+
+# Function to create database table if not already created
+def create_table():
+    with closing(connect().cursor()) as cursor:
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS vinylCollection (
+                albumID INTEGER PRIMARY KEY AUTOINCREMENT,
+                albumName TEXT NOT NULL,
+                albumArtist TEXT NOT NULL,
+                albumGenre TEXT NOT NULL,
+                albumYear TEXT NOT NULL,
+                albumPrice REAL NOT NULL
+            );
+        ''')
+
+# Function to add vinyl to the database
+def add_vinyl_to_db(album_Name, album_Artist, album_Genre, album_Year, album_Price):
+    connection = connect()
+    try:
+        with closing(connection.cursor()) as cursor:
+            cursor.execute('''
+                INSERT INTO vinylCollection (albumName, albumArtist, albumGenre, albumYear, albumPrice)
+                VALUES (?, ?, ?, ?, ?);
+            ''', (album_Name, album_Artist, album_Genre, album_Year, album_Price))
+        connection.commit()  # Commit changes to the database
+    except Exception as e:
+        print(f"Error adding vinyl to the database: {e}")
+    finally:
+        close(connection)
+
+# Function to query database for vinyl and then add them to the vinyl list using the Vinyl class
+def get_all_vinyls_from_db():
+    with closing(connect().cursor()) as cursor:
+        cursor.execute('''
+            SELECT * FROM vinylCollection;
+        ''')
+        records = cursor.fetchall()
+        vinyls = [Vinyl(*record[1:]) for record in records]  # Exclude the first element (albumID)
+        return vinyls
+    
+# Function to import collection information from a .CSV (specifically a Discogs export)
+def import_csv_to_db(csv_file_name):
+    csv_file_path = os.path.join(os.getcwd(), csv_file_name)
+    try:
+        with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            for row in csv_reader:
+                # Extract relevant information from the CSV columns
+                artist = row.get('Artist', '')
+                title = row.get('Title', '')
+                released = row.get('Released', '')
+
+                # Add the extracted information to the database
+                add_vinyl_to_db(title, artist, '', released, '')
+    except Exception as e:
+        print(f"Error importing data from CSV: {e}")
--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
-from business import Vinyl, VinylCollection
-#from db import VinylDatabase
+from business import VinylCollection
+import db
+
 
 #region -- MENU ELEMENTS --
 def display_title():
@@ -8,11 +9,9 @@ def display_title():
 def display_separator_thick():
     print("=" *56)
 
-def display_separator_thin():
-    print("-" *70)
-
 def display_menu():
-    print("\nMENU OPTIONS\n1 - Add Vinyl to Collection\n2 - View Collection\n3 - View Price Data\n4 - Select Random Vinyl\n5 - Show Menu Options\n6 - Exit Program")
+    print("\nMENU OPTIONS\n1 - Add Vinyl To Collection\n2 - View Collection\n3 - View Price Data\n4 - Select Random Vinyl\n5 - Import from CSV\n6 - Show Menu Options\n7 - Exit Program")
+
 
 
 #endregion
@@ -20,7 +19,6 @@ def display_menu():
 #region -- MAIN FUNCTION
 def main():
     vinyl_collection = VinylCollection()
-    #vinyl_database = VinylDatabase()
 
     display_separator_thick()
     display_title()
@@ -30,21 +28,31 @@ def main():
     while True:
         menu_option = int(input("Menu Option: "))
         if menu_option == 1:
+            # Get the last added vinyl from the collection and add the vinyl to the database
             vinyl_collection.add_vinyl()
+            added_vinyl = vinyl_collection.vinyls[-1]
+            db.add_vinyl_to_db(added_vinyl.albumName, added_vinyl.albumArtist, added_vinyl.albumGenre,
+                               added_vinyl.albumYear, added_vinyl.albumPrice)
         elif menu_option == 2:
+                # Retrieve all vinyl from the database and update the collection
+            vinyl_collection.vinyls = db.get_all_vinyls_from_db()
             vinyl_collection.view_collection()
         elif menu_option == 3:
             vinyl_collection.view_price_data()
         elif menu_option == 4:
             vinyl_collection.select_random_vinyl()
+            # Import data from a CSV file into the database
         elif menu_option == 5:
-            display_menu()
+            csv_file_name = input("Enter the name of the .csv file: ")
+            db.import_csv_to_db(csv_file_name)
         elif menu_option == 6:
+            display_menu()
+        elif menu_option == 7:
+            print("\nHappy listening. Goodbye! :)")
             break
         else:
             print("Invalid menu option. Please choose a valid option.")
-
-
+    
 #endregion
 
 if __name__ == "__main__":
